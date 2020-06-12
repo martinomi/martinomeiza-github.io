686c1f74fa33330a65f354a53a3cf32827be580c
martinomi <35002859+martinomi@users.noreply.github.com>
2020-06-12 14:33:37 -0400

    Add files via upload

---
+++ build/precache-manifest.49bed1de4c457f236dedd259a9b6f0de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb6fe8e89190672e308ffa2baab56d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "480e52dc2794371044c5",
+    "url": "/static/css/main.4234a16e.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480e52dc2794371044c5",
+    "url": "/static/js/main.515e399e.chunk.js"
+  },
+  {
+    "revision": "e24b2b29b0973aba011d",
+    "url": "/static/js/runtime-main.a4832251.js"
+  },
+  {
+    "revision": "b1347981632e3677a2bbf68cb16569a7",
+    "url": "/static/media/cold1.b1347981.jpg"
+  },
+  {
+    "revision": "5a7d65993e7c917edc2622062a8e24a0",
+    "url": "/static/media/warm.5a7d6599.jpg"
+  }
+]);
